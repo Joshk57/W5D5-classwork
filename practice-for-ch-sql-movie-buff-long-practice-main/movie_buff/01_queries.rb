0ccb3e401c84af0f9993ef8fb3732841452cd18d
joshua <joshk.5793@gmail.com>
2023-04-07 13:19:26 -0400

    finished first 2

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
 
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,6 +21,9 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+
+  # Movie.select(:id, :title).join(:castings).group(actor.name = "Harrison Ford").having(casting.ord != 1)
+  Movie.select(:id, :title).joins(:actors).where(actors: {name: "Harrison Ford"}).where.not(castings: {ord: 1})
 
 end
 
@@ -37,6 +41,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
+ 
   
 end
 
