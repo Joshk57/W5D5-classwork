9e0a6ab4d852f46d0dc7a5408ff18213a4cdb7ab
kcho760 <kcho760@gmail.com>
2023-04-07 16:04:23 -0400

    Finished cast list

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -18,7 +18,11 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+  .joins(:movies)
+  .where("title = ?", title)
+  .order("ord ASC")
+  .select(:id, :name)
 end
 
 def vanity_projects
